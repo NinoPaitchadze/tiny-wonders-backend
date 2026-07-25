dd223239cf5c2b7af23ce5b5c5872bc0769a5d29
NinoPaitchadze <nino.paichadze2003@gmail.com>
2026-07-26 01:10:30 +0400

    feat: implement comments with signalR real-time updates

---
+++ TinyWondersAPI/Hubs/CommentHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TinyWondersAPI.Hubs;
+
+public class CommentHub : Hub
+{
+    public async Task JoinArticle(string articleId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, articleId);
+    }
+
+    public async Task LeaveArticle(string articleId)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, articleId);
+    }
+}
